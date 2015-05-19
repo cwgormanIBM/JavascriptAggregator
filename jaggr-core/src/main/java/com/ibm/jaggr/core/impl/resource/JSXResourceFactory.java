886b3914462e30659c7b18f70893b86a3d76bf27
Cory W Gorman <cwgorman@us.ibm.com>
2015-05-19 15:21:48 -0400

    Method to check extension for jsx

--- jaggr-core/src/main/java/com/ibm/jaggr/core/impl/resource/JSXResourceFactory.java
+++ jaggr-core/src/main/java/com/ibm/jaggr/core/impl/resource/JSXResourceFactory.java
@@ -2,6 +2,9 @@ package com.ibm.jaggr.core.impl.resource;
 
 import com.ibm.jaggr.core.impl.resource.FileResourceFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class JSXResourceFactory extends FileResourceFactory {
 
 	public JSXResourceFactory() {
@@ -10,5 +13,21 @@ public class JSXResourceFactory extends FileResourceFactory {
 
 	protected JSXResourceFactory(ClassLoader classLoader) {
 		super(classLoader);
+	}
+
+	protected URI convertToTransformURI (URI uri) {
+		String fullUri = uri.toString();
+		String extension = fullUri.substring(fullUri.lastIndexOf('.'));
+		if ("js".equals(extension)) { //$NON-NLS-1$
+			try {
+				return new URI(fullUri + 'x'); //js -> jsx
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 }
